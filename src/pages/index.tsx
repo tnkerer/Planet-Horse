94f8742a6990c041f2ea7a1bf388f6961b8c6f04
d-freitas <davi.israel01@gmail.com>
2022-02-19 13:31:49 -0300

    refactor: center components in land rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import Landpage from './landpage'
-import Gameplay from './landpage/components/Gameplay'
-import NFTHorses from './landpage/components/NFTHorses'
-import Pattern from './utils/factories/pattern'
 
 function App () {
   return (
     <>
       <Landpage />
-      <Pattern type='light'>
-        <Gameplay />
-        <NFTHorses />
-      </Pattern>
     </>
   )
 }
--- src/pages/landpage/index.tsx
+++ src/pages/landpage/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Pattern from '../utils/factories/pattern'
 import Banner from './components/Banner'
+import Gameplay from './components/Gameplay'
+import NFTHorses from './components/NFTHorses'
 
 const Landpage: React.FC = () => {
   return (
     <>
       <Banner />
+      <Pattern type='light'>
+        <Gameplay />
+        <NFTHorses />
+      </Pattern>
     </>
   )
 }
