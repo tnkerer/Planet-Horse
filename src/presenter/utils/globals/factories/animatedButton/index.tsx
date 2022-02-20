6b651994bf0d2356437e0738a66f25d9bdea3965
d-freitas <davi.israel01@gmail.com>
2022-02-20 19:39:27 -0300

    refactor: add mouse-down and mouse-up state event

--- src/presenter/utils/globals/factories/animatedButton/index.tsx
+++ src/presenter/utils/globals/factories/animatedButton/index.tsx
@@ -1,37 +1,63 @@
 import React, { useState } from 'react'
+import Router from 'next/router'
 
 interface Props {
   to: string
   id: string
   inactive: string
   active: string
+  click?: string
 }
 
 const AnimatedButton: React.FC<Props> = ({
   to,
   id,
   inactive,
-  active
+  active,
+  click
 }) => {
-  const [state, setState] = useState(true)
+  const [onMouseLeaveOver, setOnMouseLeaveOver] = useState(true)
+  const [onMouseDownUp, setOnMouseDownUp] = useState(false)
 
   return (
     <>
-      <button
-        id={id}
-        className={active}
-        onMouseLeave={() => {
-          setState(true)
-        }}
-      />
+      {click
+        ? <button
+          id={id}
+          className={onMouseDownUp ? click : active}
+          onMouseLeave={() => {
+            setOnMouseLeaveOver(true)
+          }}
+          onMouseUp={() => {
+            setOnMouseDownUp(false)
+          }}
+          onMouseDown={() => {
+            setOnMouseDownUp(true)
+          }}
+          onClick={() => {
+            Router.push(to)
+          }}
+        />
+        : <button
+          id={id}
+          className={onMouseDownUp ? click : active}
+          onMouseLeave={() => {
+            setOnMouseLeaveOver(true)
+          }}
+          onClick={() => {
+            Router.push(to)
+          }}
+      />}
       <button
         id={id}
         className={inactive}
         onMouseOver={() => {
-          setState(false)
+          setOnMouseLeaveOver(false)
         }}
         style={{
-          display: state ? 'block' : 'none'
+          display: onMouseLeaveOver
+            ? 'block'
+            : 'none'
         }}
       />
     </>
