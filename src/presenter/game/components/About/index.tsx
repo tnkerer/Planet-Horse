44877044f579165ba9ee99b4e21229230c0acf8f
d-freitas <davi.israel01@gmail.com>
2022-02-21 09:49:35 -0300

    feat: add about card

---
+++ src/presenter/game/components/About/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+import brownHorse from '@/assets/game/home-tab/brown-horse.gif'
+import whiteHorse from '@/assets/game/home-tab/white-horse.gif'
+
+const About: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.content}>
+          <div className={styles.sticker}>
+            <Image
+              src={brownHorse}
+              width={320}
+              height={320}
+            />
+          </div>
+          <div className={styles.description}>
+            <h1 className={styles.title}>what is planethorse</h1>
+            <h3 className={styles.explanation}>
+              PlanetHorse is a game centered around different racehorses where you compete with competitors in search of the incredible Phorse coin
+            </h3>
+          </div>
+          <div className={styles.sticker}>
+            <Image
+              src={whiteHorse}
+              width={320}
+              height={320}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
