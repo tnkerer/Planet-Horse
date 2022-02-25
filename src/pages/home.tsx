81d1671a0329309f1660401458481ae796bdecea
d-freitas <davi.israel01@gmail.com>
2022-02-25 19:51:10 -0300

    fix: prevent navbar from overlapping page elements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,19 +7,22 @@ import Pattern from '@/utils/components/pattern'
 import BlackHorseWarning from '@/components/home/black-horse-warning'
 import Options from '@/components/home/options'
 import Footer from '@/utils/components/footer'
+import PageContent from '@/utils/components/page-content'
 
 function Home () {
   return (
     <>
       <Navbar />
-      <Banner />
-      <Board />
-      <Pattern type='white'>
-        <About />
-        <BlackHorseWarning />
-        <Options />
-        <Footer copyrightTextColor='#000' />
-      </Pattern>
+      <PageContent>
+        <Banner />
+        <Board />
+        <Pattern type='white'>
+          <About />
+          <BlackHorseWarning />
+          <Options />
+          <Footer copyrightTextColor='#000' />
+        </Pattern>
+      </PageContent>       
     </>
   )
 }
