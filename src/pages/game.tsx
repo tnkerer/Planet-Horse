32e2addb32c626a4fba9b864e08787e444ee899f
d-freitas <davi.israel01@gmail.com>
2022-03-17 11:48:35 -0300

    feat: add responsive to CardOptions from game

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -4,18 +4,21 @@ import Navbar from '@/utils/components/navbar'
 import Footer from '@/utils/components/footer'
 import CardOptions from '@/components/game/CardOptions'
 import Horses from '@/components/game/Horses'
+import PageContent from '@/utils/components/page-content'
 
 const Game: React.FC = () => {
   return (
     <>
       <Navbar />
-      <Pattern type="yellow">
-        <CardOptions />
-      </Pattern>
-      <Pattern type="light">
-        <Horses />
-        <Footer copyrightTextColor="white" />
-      </Pattern>
+      <PageContent>
+        <Pattern type="yellow">
+          <CardOptions />
+        </Pattern>
+        <Pattern type="light">
+          <Horses />
+          <Footer copyrightTextColor="white" />
+        </Pattern>
+      </PageContent>
     </>
   )
 }
