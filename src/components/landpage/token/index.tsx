666a1762428e7a0943e21260111f38175c641cf2
d-freitas <davi.israel01@gmail.com>
2022-04-13 19:58:02 -0300

    feat: add token to landpage

--- src/components/landpage/token/index.tsx
+++ src/components/landpage/token/index.tsx
@@ -1,65 +1,41 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ScrollYValueContext } from '@/utils/providers/scroll-y-value'
 import styles from './styles.module.scss'
 
-interface Props {
-  scrollValueToAnimate?: number
-}
+import TitleLayer from '@/components/landpage/title-layer'
+import LogoImage from '@/assets/utils/logos/animted-phorse-coin.gif'
 
-const Token: React.FC<Props> = ({ scrollValueToAnimate }) => {
-  const [scrolled, setScrolled] = useState(false)
-  const { scrollY } = useContext(ScrollYValueContext)
-  const [screenWidth, setScreenWidth] = useState(0)
+import Image from 'next/image'
 
-  useEffect(() => {
-    const elementAppearsToUser = scrollY >= scrollValueToAnimate
-    const screenWidthResolution = window.innerWidth
-    elementAppearsToUser && setScrolled(true)
-    setScreenWidth(screenWidthResolution)
-  }, [scrollY])
-
+const Token: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <span
-          className={styles.title}
-          style={{
-            opacity: scrolled || screenWidth <= 810 ? 1 : 0
-          }}
-        >
-          Token
-        </span>
-        <div className={styles.coinContainer}>
-          <span
-            className={styles.info}
-            style={{
-              marginLeft: scrollY >= 3420 ? '1%' : '10%',
-              opacity: scrollY >= 3420 ? '1' : '0'
-            }}
-          >
-            Token: PHORSE<br />
-            Name: Planet Horse Token PHORSE<br />
-            Network: Binance Smart Chain<br />
-            Max supply: 100.000.000
-          </span>
-          <div
-            className={styles.coinImage}
-            style={{
-              opacity: scrollY >= 3420 ? '1' : '0'
-            }}
-          ></div>
+      <div className={styles.container_header} />
+
+      <div className={styles.container_content}>
+        <div className={styles.content_title}>
+          <TitleLayer>
+            Token
+          </TitleLayer>
         </div>
-        <span
-          className={styles.description}
-          style={{
-            opacity: scrollY >= 3534 ? '1' : '0'
-          }}
-        >
-          The PHORSE Token is the native game currency. It allows the investor to play, invest and also trade the token.
-          <br />
-          <br />
-          There is a fixed total amount of 100 million PHORSE tokens.
-        </span>
+        <div className={styles.content_information}>
+          <div className={styles.information_text}>
+            <div className={styles.text_title}>
+              <u>PHORSE TOKEN</u> 
+            </div>
+            <div className={styles.text_description}>
+              Symbol: PHORSE<br />
+              Name: PlanetHorse<br />
+              Token Max Supply: 100,000,000<br />
+              Chain: BSC Mainnet<br />
+              Contract: Coming Soon
+            </div>
+          </div>
+          <div className={styles.information_icon}>
+            <div className={styles.icon_container}>
+              <Image src={LogoImage} layout='fill' />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
