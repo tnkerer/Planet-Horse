de8410cc4bafc20d719ecc980beb6ab25ad82aff
d-freitas <davi.israel01@gmail.com>
2022-04-14 14:50:35 -0300

    feat: add graph vector to title

--- src/components/landpage/title-layer/index.tsx
+++ src/components/landpage/title-layer/index.tsx
@@ -3,20 +3,20 @@ import styles from './styles.module.scss'
 
 const TitleLayer: React.FC = ({ children }) => {
   return (
-    <span className={styles.container}>
-      <div className={`
-        ${styles.container_content}
-        ${styles.animation}
-      `}>
-        <span className={styles.content_text}>
-          {children}
-        </span>
-        <span className={styles.content_line__horizontal} />
-        <span className={styles.content_line__diagonal}>
-          <span className={styles.diagonal_circle} />
-        </span>
+    <div className={`${styles.container} ${styles.animation}`}>
+      <div className={styles.container_content}>
+        <div className={styles.content_title}>
+          <svg height='80' width='100%'>
+            <path d='M 10 20 L 80 80' stroke='#fff' strokeWidth='1' />
+            <circle cx='10' cy='20' r='5' fill='#fff' />
+            <path d='M 81 79 l 900 0' stroke='#fff' strokeWidth='1' />
+          </svg>
+          <span className={styles.content_text}>
+            {children}
+          </span>
+        </div>
       </div>
-    </span>
+    </div>
   )
 }
 
