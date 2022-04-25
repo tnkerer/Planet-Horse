e1c4ab06f6cd4f435d1eb8e60c5fb77235ee68a9
railsonsergio <railsonsergio.dev@gmail.com>
2022-04-25 20:29:12 -0300

    refactor: social midia landpage

--- src/components/landpage/presentation/index.tsx
+++ src/components/landpage/presentation/index.tsx
@@ -3,14 +3,36 @@ import styles from './styles.module.scss'
 
 import Link from 'next/link'
 
+import discordIcon from '@/assets/icons/socials/discord.webp'
+import twitterIcon from '@/assets/icons/socials/twitter.webp'
+import telegramIcon from '@/assets/icons/socials/telegram.webp'
+
 const Presentation: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.container_icon} />
       <div className={styles.container_button}>
         <span>•</span>
-        <Link href='#'>PRESS TO EXPLORE</Link>
+        <Link href='#'>Press to explorer</Link>
         <span>•</span>
+      </div>
+
+      <div className={styles.social_media}>
+        <Link href='https://discord.gg/3EDMdSYUXs'>
+          <a target="_blank" rel="noreferrer">
+            <img src={discordIcon.src} alt="Discord Planet Horse" />
+          </a>
+        </Link>
+        <Link href='https://www.t.me/planethorse'>
+          <a target="_blank" rel="noreferrer">
+            <img src={telegramIcon.src} alt="Telegram Planet Horse" />
+          </a>
+        </Link>
+        <Link href='https://twitter.com/PlanetHorseNFT'>
+          <a target="_blank" rel="noreferrer">
+            <img src={twitterIcon.src} alt="Twitter Planet Horse" />
+          </a>
+        </Link>
       </div>
     </div>
   )
