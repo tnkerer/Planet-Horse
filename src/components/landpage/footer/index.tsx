ee74a5bdc79a9f82de8f389e269eeb1599874414
railsonsergio <railsonsergio.dev@gmail.com>
2022-04-25 17:30:26 -0300

    refactor: footer landpage

--- src/components/landpage/footer/index.tsx
+++ src/components/landpage/footer/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 import Image from 'next/image'
-import Link from 'next/link'
+
+import discordIcon from '@/assets/icons/socials/discord.webp'
+import twitterIcon from '@/assets/icons/socials/instagram.webp'
+import telegramIcon from '@/assets/icons/socials/telegram.webp'
+import instagramIcon from '@/assets/icons/socials/twitter.webp'
 
 import planethorseLogoImage from '@/assets/landpage/planethorse-logo-sem-ferradura.webp'
 import horseSpriteImage from '@/assets/landpage/horse-sprite.webp'
@@ -10,30 +14,39 @@ import horseSpriteImage from '@/assets/landpage/horse-sprite.webp'
 const Footer: React.FC = () => {
   return (
     <footer className={styles.container}>
-      <div className={styles.container_marketing}>
-        <Image
-          src={planethorseLogoImage}
-          width={292}
-          height={118}
-        />
-        <div className={styles.marketing_community__link}>
-          <span>•</span>
-          <Link href='#'>COMMUNITY</Link>
-          <span>•</span>
+      <div className={styles.container_topside}>
+        <div className={styles.container_marketing}>
+          <Image
+            src={planethorseLogoImage}
+            width={292}
+            height={118}
+          />
+          <div className={styles.marketing_community__link}>
+            <p><span>•</span>COMMUNITY<span>•</span></p>
+          </div>
+
+          <div className={styles.social_media}>
+            <img src={discordIcon.src} alt="Discord Planet Horse" />
+            <img src={twitterIcon.src} alt="Twitter Planet Horse" />
+            <img src={instagramIcon.src} alt="Instagram Planter Horse" />
+            <img src={telegramIcon.src} alt="Telegram Planet Horse" />
+          </div>
+        </div>
+
+        <div className={styles.container_partner}>
+          <div className={styles.partner_title}>PARTNER</div>
+          <Image
+            src={horseSpriteImage}
+            width={204}
+            height={210}
+          />
+          <div className={styles.partner_persons}>SOON</div>
         </div>
       </div>
-      <div className={styles.container_partner}>
-        <div className={styles.partner_title}>PARTNER</div>
-        <Image
-          src={horseSpriteImage}
-          width={204}
-          height={210}
-        />
-        <div className={styles.partner_persons}>SOON</div>
-      </div>
+
       <div className={styles.container_footer}>
         <span className={styles.footer_copyright}>
-          COPYRIGHT @ 2022 PLANETHORSE
+          Copyright © 2022 planethorse
         </span>
       </div>
     </footer>
