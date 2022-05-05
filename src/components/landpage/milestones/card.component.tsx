df340e1e14b962e72b2f418894bf41efe4e20f78
railsonsergio <railsonsergio.dev@gmail.com>
2022-05-05 18:04:56 -0300

    refactor: milestones

---
+++ src/components/landpage/milestones/card.component.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+import Image, { ImageProps } from 'next/image'
+
+import { useIsVisible } from '@/utils/hooks/is-visible'
+
+import styles from './styles.module.scss'
+
+type Props = {
+  title: string;
+  value?: number;
+  icon: ImageProps['src'];
+  animate: boolean;
+}
+
+const Card: React.FC<Props> = ({ title, value = 20000, icon, animate }) => {
+  const [ascendingNumber, setAscendingNumber] = useState(0)
+
+  useEffect(() => {
+    if (animate) {
+      let i = 0
+
+      const ascender = setInterval(() => {
+        (i === value) && clearInterval(ascender)
+        setAscendingNumber(i)
+        i = + i + 25
+      }, 2)
+    }
+  }, [animate])
+
+  return (
+    <div className={styles.cards_card}>
+      <span className={styles.card_image}>
+        <Image layout='fill' src={icon} />
+      </span>
+      <span className={styles.card_title}>{title}</span>
+      <span className={styles.card_value}>
+        {ascendingNumber !== value ? ascendingNumber : 'Soon'}
+      </span>
+    </div>
+  )
+}
+
+export default Card
--- src/components/landpage/milestones/index.tsx
+++ src/components/landpage/milestones/index.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
-import styles from './styles.module.scss'
-
-import Image from 'next/image'
+import React, {  useRef } from 'react'
 
 import { useIsVisible } from '@/utils/hooks/is-visible'
 
@@ -11,71 +8,33 @@ import volume_icon from '@/assets/landpage/volume-status.webp'
 
 import TitleLayer from '@/components/landpage/title-layer'
 
+import Card from './card.component'
+import styles from './styles.module.scss'
+
+
 const Milestones: React.FC = () => {
   const myRef = useRef()
-  const [ascendingNumber, setAscendingNumber] = useState(0)
   const isVisible = useIsVisible(myRef)
 
-  useEffect(() => {
-    if (isVisible) {
-      let i = 0
-
-      const ascender = setInterval(() => {
-        (i === 20000) && clearInterval(ascender)
-        setAscendingNumber(i)
-        i = + i + 25
-      }, 2)
-    }
-  }, [isVisible])
+  const CARDS = [
+    { id: 'card-wallets', title: 'Wallets', icon: wallet_icon, value: 20000 }, 
+    { id: 'card-horses', title: 'Horses', icon: horses_icon, value: 20000 },
+    { id: 'card-volume', title: 'Volume', icon: volume_icon, value: 20000 }
+  ]
 
   return (
-    <div className={styles.container}>
-      <TitleLayer>
-        Milestones
-      </TitleLayer>
-      <div className={`
-        ${styles.container_cards}
-        ${isVisible && styles.animation}
-      `}>
-        <div className={styles.cards_card} ref={myRef}>
-          <span className={styles.card_image}>
-            <Image layout='fill' src={wallet_icon} />
-          </span>
-          <span className={styles.card_title}>
-            WALLETS
-          </span>
-          <span className={styles.card_value}>
-            {ascendingNumber !== 20000 ? ascendingNumber : 'SOON'}
-          </span>
-        </div>
-        <div className={`
-          ${styles.cards_card}
-        `}>
-          <span className={styles.card_image}>
-            <Image layout='fill' src={horses_icon} />
-          </span>
-          <span className={styles.card_title}>
-            HORSES
-          </span>
-          <span className={styles.card_value}>
-            {ascendingNumber !== 20000 ? ascendingNumber : 'SOON'}
-          </span>
-        </div>
-        <div className={`
-          ${styles.cards_card}
-        `}>
-          <span className={styles.card_image}>
-            <Image layout='fill' src={volume_icon} />
-          </span>
-          <span className={styles.card_title}>
-            VOLUME
-          </span>
-          <span className={styles.card_value}>
-            {ascendingNumber !== 20000 ? ascendingNumber : 'SOON'}
-          </span>
-        </div>
+    <section className={styles.container}>
+      <TitleLayer>Milestones</TitleLayer>
+      <div
+        ref={myRef}
+        className={`
+          ${styles.container_cards}
+          ${isVisible && styles.animation}
+        `}
+      >
+       {CARDS.map(card => <Card key={card.id} {...card} animate={isVisible} />)}
       </div>
-    </div>
+    </section>
   )
 }
 
