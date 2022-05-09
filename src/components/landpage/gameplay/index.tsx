e95c0eff63d695738adb928e1f4be12690847bf2
railsonsergio <railsonsergio.dev@gmail.com>
2022-05-09 13:27:16 -0300

    refactor: gameplay

--- src/components/landpage/gameplay/index.tsx
+++ src/components/landpage/gameplay/index.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from 'react'
-import styles from './styles.module.scss'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -15,91 +14,107 @@ import noiseTv from '@/assets/landpage/chiado.gif'
 import whitepaperImage from '@/assets/landpage/whitepaper.webp'
 import whitepaperHoverImage from '@/assets/landpage/whitepaper-mouse.webp'
 import arboresImage from '@/assets/landpage/arbores.webp'
-import gloves from '@/assets/landpage/luvas.gif'
 
-const Gameplay: React.FC = () => {
+import styles from './styles.module.scss'
+
+
+const CHANNELS = {
+  racing: {
+    title: 'Racing',
+    content: `In the Race Mode you can play PvP or PvC.
+      The rewards for the winners will be: experience points, materials, items and tokens.`,
+    image: racingImage,
+  },
+  buy_sell: {
+    title: 'Buy/Sell',
+    content: `In the market, players can buy/sell horses, materials, items and stables.
+      Players will spend/earn PlanetHorse token when trading on the market.`,
+    image: buySell,
+  },
+  upgrade: {
+    title: 'Upgrade',
+    content: `You can pay in PHORSE currency to upgrade your horse.
+      Increase your win rates and earn more. Horse upgrade increases speed, sprint, support and power.`,
+    image: upgrade,
+  },
+}
+
+type Channel = keyof typeof CHANNELS
+
+const GamePlay: React.FC = () => {
   const myRef = useRef()
   const isVisible = useIsVisible(myRef)
 
-  const racingText = 'In the Race Mode you can play PvP or PvC. The rewards for the winners will be: experience points, materials, items and tokens.'
-  const buySellText = 'In the market, players can buy/sell horses, materials, items and stables. Players will spend/earn PlanetHorse token when trading on the market.'
-  const upgradeText = 'You can pay in PHORSE currency to upgrade your horse. Increase your win rates and earn more. Horse upgrade increases speed, sprint, support and power.'
+  const [urlIsVisible, setUrlIsVisible] = useState(CHANNELS.racing.image)
+  const [textIsVisible, setTextIsVisible] = useState(CHANNELS.racing.content)
 
-  const [urlIsVisible, setUrlIsVisible] = useState(racingImage)
-  const [textIsVisible, setTextIsVisible] = useState(racingText)
+  function changeTvChannel (toChannel: Channel) {
+    const channel = CHANNELS[toChannel];
 
-  function changeTvChannel () {
     setUrlIsVisible(noiseTv)
-  }
-
-  function handleClickRacing () {
-    changeTvChannel()
-    setTimeout(() => setUrlIsVisible(racingImage), 500)
-    setTextIsVisible(racingText)
-  }
-  function handleClickUpgrade () {
-    changeTvChannel()
-    setTimeout(() => setUrlIsVisible(upgrade), 500)
-    setTextIsVisible(upgradeText)
-  }
-  function handleClickBuySell () {
-    changeTvChannel()
-    setTimeout(() => setUrlIsVisible(buySell), 500)
-    setTextIsVisible(buySellText)
+    setTextIsVisible(channel.content)
+    setTimeout(() => setUrlIsVisible(channel.image), 500)
   }
 
   return (
-    <div className={styles.container} ref={myRef}>
-      <div className={`
-        ${styles.container_content}
-        ${isVisible && styles.animation}
-      `}>
-        <slot className={styles.content_gameplay}>
-          <div className={styles.gameplay_line__vertical} />
-          <div className={styles.gameplay_content}>
-            <div className={styles.content_head}>
-              <h1 className={styles.head_title}>GAMEPLAY</h1>
+    <section className={styles.container} ref={myRef}>
+      <div className={`${styles.container_content} ${isVisible && styles.animation}`}>
+        <div className={styles.content_gameplay}>
+          <div>
+            <header className={styles.content_head}>
+              <h1>Game Play</h1>
+            </header>
 
-              <u className={styles.head_option} onClick={handleClickRacing}>RACING</u>
-              <u className={styles.head_option} onClick={handleClickBuySell}>BUY / SELL</u>
-              <u className={styles.head_option} onClick={handleClickUpgrade}>UPGRADE</u>
-            </div>
+            <ul className={styles.gameplay_topics}>
+              {Object.entries(CHANNELS).map(([key , channel]) => (
+                <li
+                  key={key}
+                  about={channel.title}
+                  className={styles.toggle}
+                  onClick={() => changeTvChannel(key as Channel)}
+                >
+                  <u>{channel.title}</u>
+                </li>
+              ))}
+            </ul>
+
             <div className={styles.gameplay_body}>
-              <span className={styles.gameplay_description__paragraph1}>
-                {textIsVisible}
-              </span>
+              <p>{textIsVisible}</p>
             </div>
           </div>
-        </slot>
-        <slot className={styles.content_horses}>
+        </div>
+        
+        <div className={styles.content_horses}>
           <div className={styles.horses_content}>
+
             <div className={styles.content_viewfinder}>
               <Image layout='fill' src={urlIsVisible} />
             </div>
+
             <div className={styles.content_tv}>
               <Image layout='fill' src={tvImage} />
             </div>
-            <div className={styles.content_btn__touch}>
+
+            <div className={styles.link}>
               <Link href='https://whitepaper.planethorse.me/'>
                 <a target='_blank' />
               </Link>
             </div>
-            <div className={styles.content_btn}>
-              <div className={styles.btn_book__active}>
-                <Image layout='fill' src={whitepaperHoverImage} />
-              </div>
-              <div className={styles.btn_book}>
-                <Image layout='fill' src={whitepaperImage} />
-              </div>
+
+            <div className={styles.book_container}>
+              <Image layout='fill' src={whitepaperImage} className={styles.book} />
+              <Image layout='fill' src={whitepaperHoverImage} className={styles.book_hover} />
             </div>
+
+            
             <div className={styles.content_bush}>
-              <Image src={arboresImage} />
+              <Image src={arboresImage} layout='fill' />
             </div>
           </div>
-        </slot>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
-export default Gameplay
+export default GamePlay
