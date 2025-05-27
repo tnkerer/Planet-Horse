a39ffd7fd74e8a60feab14305bb39423cfffb28e
zyantes <phillipe.o.m@hotmail.com>
2025-05-27 00:13:09 -0300

    feat: carousel feat added to mobile cards option

--- src/components/game/CardOptions/index.tsx
+++ src/components/game/CardOptions/index.tsx
@@ -13,36 +13,36 @@ interface Props {
 const CardOptions: React.FC<Props> = ({ changeView }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.cards}>
-        <div className={styles.card}>
-          <Image layout="fill" src={levelGif} alt="card-level" width={200} height={200} />
-          <button
-            className={styles.buyButton}
-          >LEVEL UP</button>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <Image layout="fill" src={levelGif} alt="card-level" width={200} height={200} />
+            <button
+              className={styles.buyButton}
+            >LEVEL UP</button>
+          </div>
+          <div className={styles.card}>
+            <Image layout="fill" src={chestGif} alt="card-chest" width={200} height={200} />
+            <button
+              className={styles.buyButton}
+              /* onClick={setPopUp('chest')} */
+              onClick={() => changeView('items')}
+            >BUY CHESTS</button>
+          </div>
+          <div className={styles.card}>
+            <Image layout="fill" src={horseFusionGif} alt="card-breed" width={200} height={200} />
+            <button
+              className={styles.fusionButton}
+            /* onClick={setPopUp('fusion')} */
+            >Coming Soon...</button>
+          </div>
+          <div className={styles.card}>
+            <Image layout="fill" src={upgradeStableGif} alt="card-upgrade" width={200} height={200} />
+            <button
+              className={styles.upgradeButton}
+            /* onClick={setPopUp('upgrade')} */
+            >Coming Soon...</button>
+          </div>
         </div>
-        <div className={styles.card}>
-          <Image layout="fill" src={chestGif} alt="card-chest" width={200} height={200} />
-          <button
-            className={styles.buyButton}
-            /* onClick={setPopUp('chest')} */
-            onClick={() => changeView('items')}
-          >BUY CHESTS</button>
-        </div>
-        <div className={styles.card}>
-          <Image layout="fill" src={horseFusionGif} alt="card-breed" width={200} height={200} />
-          <button
-            className={styles.fusionButton}
-          /* onClick={setPopUp('fusion')} */
-          >Coming Soon...</button>
-        </div>
-        <div className={styles.card}>
-          <Image layout="fill" src={upgradeStableGif} alt="card-upgrade" width={200} height={200} />
-          <button
-            className={styles.upgradeButton}
-          /* onClick={setPopUp('upgrade')} */
-          >Coming Soon...</button>
-        </div>
-      </div>
     </div>
   )
 }
