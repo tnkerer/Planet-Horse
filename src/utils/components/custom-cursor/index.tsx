0508a17512dcd5743bc3f33942f0707acd6e00dd
zyantes <phillipe.o.m@hotmail.com>
2025-06-10 19:26:41 -0300

    fix: build error after cursor feature

--- src/utils/components/custom-cursor/index.tsx
+++ src/utils/components/custom-cursor/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './cursor.module.scss';
 
 export default function CustomCursor() {
