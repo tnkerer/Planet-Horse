2432b6bac787d9e0607131798e794f991985ae64
zyantes <phillipe.o.m@hotmail.com>
2025-08-07 09:44:01 -0300

    feat: added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '../styles/globals.scss'
 import { WalletProvider } from '@/contexts/WalletContext'
 import { UserProvider } from '@/contexts/UserContext'
 import CustomCursor from '@/utils/components/custom-cursor'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 function MyApp({ Component, pageProps }) {
   return (
