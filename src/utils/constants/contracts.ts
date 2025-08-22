220d7d1fa8a66a27c90776ce206eeb39bfe7bcd8
zyantes <phillipe.o.m@hotmail.com>
2025-08-21 23:27:37 -0300

    feat: add new contract

--- src/utils/constants/contracts.ts
+++ src/utils/constants/contracts.ts
@@ -1,5 +1,5 @@
 export const contracts = {
-    phorse: '0x7f8e304eb2894e57f8b930000f396319729bd1f1',
+    phorse: '0x6ad39689cac97a3e647fabd31534555bc7edd5c6',
     items: '0xd2d7074d682b4fe1f9df998251c30c08b32ec627'
 }
 
